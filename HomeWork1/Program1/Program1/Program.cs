8e0b824041d9f22cd0bfe7e69147e417457f78e4
wuzehua-ssd <1393373326@qq.com>
2018-09-09 20:14:50 +0800

    第一次作业

---
+++ HomeWork1/Program1/Program1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("输入第一个数字：");
+            string str1 = Console.ReadLine();
+            double num1 = Int32.Parse(str1);
+
+            Console.WriteLine("输入第二个数字：");
+            string str2 = Console.ReadLine();
+            double num2 = Int32.Parse(str2);
+
+            double num = num1 * num2;
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ HomeWork1/Program2/Program2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1 = Int32.Parse(textBox1.Text);
+            double num2 = Int32.Parse(textBox2.Text);
+            double num = num1 * num2;
+
+            string str1 = Convert.ToString(num);
+            textBox3.Text = str1;
+        }
+    }
+}
