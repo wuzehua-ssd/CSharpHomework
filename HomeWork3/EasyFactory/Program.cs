ef7241b1eb42c6afdf5caa886fdd3f4d8fa3fe5f
wuzehua-ssd <1393373326@qq.com>
2018-09-26 13:02:54 +0800

    第三次作业。

---
+++ HomeWork3/EasyFactory/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyFactory
+{
+    abstract class Chart
+    {
+        public Chart()
+        {
+        }
+        public abstract double Area();
+
+    }
+    //圆
+    class Circle : Chart
+    {
+        private float Radius;
+        public Circle(float r)
+        {
+            this.Radius = r;
+        }
+        public override double Area()
+        {
+            return (float)(Radius * Radius * Math.PI);
+        }
+    }
+    //三角形
+    class Trilateral : Chart
+    {
+        private float Height;
+        private float Base;
+        public Trilateral(float a, float h)
+        {
+            this.Height = h;
+            this.Base = a;
+        }
+        public override double Area()
+        {
+            return Height * Base / 2;
+        }
+    }
+    //正方形
+    class Square : Chart
+    {
+        private float Side;
+        public Square(float a)
+        {
+            Side = a;
+        }
+        public override double Area()
+        {
+            return Side * Side;
+        }
+    }
+    //长方形
+    class Rectangle : Chart
+    {
+        private float Height;
+        private float Width;
+        public Rectangle(float a, float b)
+        {
+            Height = a;
+            Width = b;
+        }
+        public override double Area()
+        {
+            return Height * Width;
+        }
+    }
+    class Factory
+    {
+        public static Chart GetChart(string cha, float s, float w=1)
+        {
+            Chart chart = null;
+            if (cha.Equals("Trilateral"))
+            {
+                chart = new Trilateral(s, w);
+            }
+            else if (cha.Equals("Rectangle"))
+            {
+                chart = new Rectangle(s, w);
+            }
+            else if (cha.Equals("Circle"))
+            {
+                chart = new Circle(s);
+            }
+            else if (cha.Equals("Square"))
+            {
+                chart = new Square(s);
+            }
+            return chart;
+        }
+    }
+    public class Test
+    {
+        public static void Main(string[] args)
+        {
+            Chart[] charts =
+            {
+             Factory.GetChart("Trilateral",5,6),
+             Factory.GetChart("Rectangle",5,6),
+             Factory.GetChart("Circle",4),
+             Factory.GetChart("Square",4)
+             };
+
+            Console.WriteLine("Shapes Collection");
+            foreach (Chart s in charts)
+            {
+                Console.WriteLine(s + " Area=" + s.Area());
+            }
+
+        }
+    }
+}
---
+++ HomeWork3/program1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class student
+    {
+        private String studentNum;
+        private String classNum;
+        private String studentName;
+        private String studentSex;
+        private int studentAge = 0;
+
+        static void Main(string[] args)
+        {
+            student newstudent = new student();
+            newstudent.studentNum = getStudentNum();
+            newstudent.classNum = getClassNum();
+            newstudent.studentName = getStudentName();
+            newstudent.studentSex = getStudentSex();
+            newstudent.studentAge = getStudentAge();
+
+            Console.WriteLine("修改年纪为：");
+            int age = Convert.ToInt32(Console.ReadLine());
+            newstudent.studentAge = setStudentAge(age);
+
+            Console.WriteLine("学号：" + newstudent.studentNum + "  班级号：" + newstudent.classNum + "  姓名：" + newstudent.studentName + "  性别：" + newstudent.studentSex + "  年纪：" + newstudent.studentAge);
+        }
+
+        static String getStudentNum()
+        {
+            Console.WriteLine("请输入学号：");
+            String studentNum = Console.ReadLine();
+            return studentNum;
+        }
+
+        static String getClassNum()
+        {
+            Console.WriteLine("请输入班级号：");
+            String classNum = Console.ReadLine();
+            return classNum;
+        }
+
+        static String getStudentName()
+        {
+            Console.WriteLine("请输入学生姓名：");
+            String studentName = Console.ReadLine();
+            return studentName;
+        }
+
+        static String getStudentSex()
+        {
+            Console.WriteLine("请输入性别：");
+            String studentSex = Console.ReadLine();
+            return studentSex;
+        }
+
+        static int getStudentAge()
+        {
+            Console.WriteLine("请输入学生年纪：");
+            int age = Convert.ToInt32(Console.ReadLine());
+            return age;
+        }
+
+        static int setStudentAge(int a)
+        {
+            return a;
+        }
+    }
+}
+
